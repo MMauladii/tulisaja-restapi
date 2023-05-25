c03b414d19d712fa4044ca546cb2146a16054849
MMauladii <muhammadmauladi@mhs.mdp.ac.id>
2023-05-25 13:17:34 +0700

    update&delete

--- routes/post.js
+++ routes/post.js
@@ -12,5 +12,50 @@ router.get('/', async(req, res) => {
         })
     }
 })
+//tambah
+router.post('/', async (req, res) => {
+    const inputPost = new Post({
+        content: req.body.content,
+        user_id: req.body.user_id,
+        username: req.body.username
+    })
+
+    try {
+        const post = await inputPost.save()
+        res.json(post) 
+    } catch (error) {
+    res.json({
+        message: error
+    })
+   }
+})
+//update
+router.put('/postId', async (req, res) => {
+    const data = {
+        content: req.body.content,
+    }
+    try {
+        const post = await Post.updateOne({
+            _id: req.params.postId
+        }, data)
+    } catch (erorr) {
+        res.json({
+            message: erorr
+        })
+    }
+})
+
+router.delete('/:postId', async (req, res) => {
+    try{
+        const post = await Post.deleteOne({
+            _id: req.params.postId
+        })
+        res.json(post)
+    } catch (erorr) {
+        res.json({
+            message: erorr
+        })
+    }
+})
 
 module.exports = router
